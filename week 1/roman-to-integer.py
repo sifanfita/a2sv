fc9501c46645cf81047fbd65afe2f9f8f3720cca
sifanfita <142254219+sifanfita@users.noreply.github.com>
2023-12-02 21:14:07 +0300

    Added solution for Roman to Integer

---
+++ week 1/roman-to-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+       roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+      }
+    
+       result = 0
+    
+       for i in range(len(s)):
+           if i<len(s)-1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+             result -= roman_values[s[i]]
+           else:
+              result += roman_values[s[i]]
+    
+       return result
+        
