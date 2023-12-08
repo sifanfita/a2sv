013c1747f4794609577091a3748e807b846543a0
sifanfita <142254219+sifanfita@users.noreply.github.com>
2023-12-08 09:47:05 +0300

    Added solution for Escape The Ghosts

---
+++ week 1/escape-the-ghosts.py
@@ -0,0 +1,14 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+       
+        pac_man_distance = abs(target[0]) + abs(target[1])
+
+        for ghost in ghosts:
+            ghost_distance = abs(ghost[0] - target[0]) + abs(ghost[1] - target[1])
+            if ghost_distance <= pac_man_distance:
+               return False
+
+        return True
+        
+       
+        
