3bf244c9b8d6255916c44b0f292dd3fa6733ba78
sifanfita <142254219+sifanfita@users.noreply.github.com>
2023-12-14 00:09:10 +0300

    Added solution for Design an Ordered Stream

---
+++ week 1/design-an-ordered-stream.py
@@ -0,0 +1,24 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream = [None] * (n + 1)  
+        self.pointer = 1  
+
+      
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey] = value
+        result = []
+
+        
+        while self.pointer < len(self.stream) and self.stream[self.pointer] is not None:
+            result.append(self.stream[self.pointer])
+            self.pointer += 1
+
+        return result
+
+        
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
