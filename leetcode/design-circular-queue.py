9a00713d8918bdbd7afc7d73f4c9c73cbbbf55b5
sifanfita <142254219+sifanfita@users.noreply.github.com>
2024-03-02 19:53:12 +0300

    Add solution for Design Circular Queue

---
+++ leetcode/design-circular-queue.py
@@ -0,0 +1,34 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.k = k
+        self.q = [0] * k
+        self.size = 0
+        self.front = 0
+        self.rear = k - 1
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.k
+        self.q[self.rear] = value
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.front]
+
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
